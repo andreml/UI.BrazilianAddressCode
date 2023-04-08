caf31700734755f89eab4a56a04f23872b3c2f9e
andremunizdelima <andremunizdelima@hotmail.com>
2023-04-08 17:34:50 -0300

    Add project files.

---
+++ UI.BrazilianAddressCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UI.BrazilianAddressCode.Models
+{
+    public class ErrorViewModel
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
